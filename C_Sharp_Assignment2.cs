e0113e35fcd420a2e04acf38ab34593486302ae9
DavidLai0927 <B22475656@gmail.com>
2015-04-23 15:40:23 +0800

    myA2

---
+++ C_Sharp_Assignment2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuickSharp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            int i;
+            for(i = 0; i < 8; i++) {
+                if ( i % 2 == 0) 
+                    System.Console.WriteLine("XOXOXOXO");
+                else
+                    System.Console.WriteLine("OXOXOXOX");
+            }
+            System.Console.WriteLine("Press any key to leave");
+            System.Console.ReadLine();//parse
+        }
+    }
+}
